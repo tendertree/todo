887357d82b2761bf4a3aa7118962b5535b001cad
tendertree <tendertree@gmail.com>
2023-04-25 13:04:01 +0900

    feat: add basic tween

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useLayoutEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import gsap from "gsap";
 
 function App() {
-	const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0);
+	useLayoutEffect(() => {
+		let ctx = gsap.context(() => {
+			gsap.fromTo(
+				"#box",
+				{
+					backgroundColor: "red",
+					scale: 0,
+				},
+				{
+					backgroundColor: "black",
+					duration: 1,
+					repeat: -1,
+					yoyo: true,
+					scale: 1,
+				}
+			);
+		});
+		return () => ctx.revert();
+	}, []);
 
 	return (
 		<>
-			<h1 className='text-3xl bold text-red-100'>hello</h1>
+			<h1 className="text-3xl bold text-red-100">hello</h1>
+			<div id="box">this is bxo</div>
 		</>
-	)
+	);
 }
 
-export default App
+export default App;
