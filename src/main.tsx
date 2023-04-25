eea78c4f63e6e551d071ab48d3520088524d28da
tendertree <tendertree@gmail.com>
2023-04-25 21:06:20 +0900

    feat: add router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ReactQueryDevtools } from 'react-query-devtools'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <div>Hello world!</div>,
+	},
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<ReactQueryDevtools />
+		<RouterProvider router={router} />
+		<App />
+	</React.StrictMode>,
 )
