daee04ba83fa72a9043fdacf4f2dd68a2a261bae
tendertree <tendertree@gmail.com>
2023-04-29 11:11:24 +0900

    fix: devtool dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { ReactQueryDevtools } from 'react-query-devtools'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
 
